aec63520325e59a884972deb6b9509dcc2a50f87
Sheila Wittman <szandora789@gmail.com>
2018-06-12 21:04:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ end
 
 def self.find_or_create_by_name(name)
   song = self.find_by_name(name)
-  song.nil ? self.create_by_name(name) : song
+    song.nil? ? self.create_by_name(name) : song
 end
 
 def self.alphabetical
